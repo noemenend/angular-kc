2ee5de9858af9038d640620640f0a495e6169344
Noelia Muñiz Menéndez <nmunizmenend@gmail.com>
2018-11-29 21:47:11 +0100

    Subida inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-kc';
+  logo = '../assets/logo.svg';
+}
